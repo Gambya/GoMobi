65da208ff8215e14be6147dbf393d36d2a086365
Roberto Lopes Ramos <robertolopesramos@gmail.com>
2016-05-03 23:56:10 -0300

    Add Views

---
+++ GoMobi/GoMobi/Views/LoginView.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace GoMobi.Views
+{
+    public class LoginView : ContentPage
+    {
+        public LoginView()
+        {
+            Content = new StackLayout
+            {
+                Children = {
+                    new Label { Text = "Login" }
+                }
+            };
+        }
+    }
+}
---
+++ GoMobi/GoMobi/Views/MapView.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace GoMobi.Views
+{
+    public class MapView : ContentPage
+    {
+        public MapView()
+        {
+            Content = new StackLayout
+            {
+                Children = {
+                    new Label { Text = "Mapa" }
+                }
+            };
+        }
+    }
+}
